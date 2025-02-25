ae8d887269f3719ab8a69aa344ff485dc4406f98
umasambath <umasambath1998@gmail.com>
2025-02-26 11:35:06 +1300

    Updated React Version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
 
 
 function App() {
@@ -10,8 +9,6 @@ function App() {
     <Router>
       <Routes>
        <Route path='/' element={<Home />} />
-       {/* <Route path="about" element={<About />} /> */}
-       {/* <Route path='/add/user' element={<AddContact />} /> */}
       </Routes>
     </Router>
     </>
